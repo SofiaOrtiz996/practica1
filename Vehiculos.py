c9a31ee247ff43706be3c6c89d86a6be6f3751ad
SofiaOrtiz996 <sofiapatricia093@gmail.com>
2026-04-18 17:51:13 -0600

    Se agrego el setter y getter de el atributo marca

--- Vehiculos.py
+++ Vehiculos.py
@@ -1,7 +1,14 @@
 class Vehiculo (self, marca, modelo, año, velocidad_actual, nivel_combustible):
     def_init_(self,marca,modelo,año, velocidad_actual,nivel_combustible):
-        self.__marca=marca
-        self.__modelo=modelo
-        self.__año=año
-        self.__velocidad_actual=velocidad_actual
-        self.__nivel_combustible=nivel_combustible
+        self.__marca= marca
+        self.__modelo= modelo
+        self.__año= año
+        self.__velocidad_actual= velocidad_actual
+        self.__nivel_combustible= nivel_combustible
+    
+    def get_marca(self):
+        return self.__marca
+    
+    def set_marca(self,marca):
+        return self.__marca= marca
+    
