7839f757167ac217235e18556db7ad3819db1b2c
SofiaOrtiz996 <sofiapatricia093@gmail.com>
2026-04-18 17:44:21 -0600

    Se agrego la funcion init y los self

--- Vehiculos.py
+++ Vehiculos.py
@@ -1,2 +1,7 @@
 class Vehiculo (self, marca, modelo, año, velocidad_actual, nivel_combustible):
-    
+    def_init_(self,marca,modelo,año, velocidad_actual,nivel_combustible):
+        self.__marca=marca
+        self.__modelo=modelo
+        self.__año=año
+        self.__velocidad_actual=velocidad_actual
+        self.__nivel_combustible=nivel_combustible
